ad0a8f03d820659ad3fb20561c54032e7e83ed4b
Marvin Bernd <marvinbernd@Marvins-MacBook-Pro.local>
2020-10-14 20:55:04 +0200

    add create button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { AddNewItem } from './AddNewItem';
+import { Card } from './Card';
+import { Column } from './Column';
 
 import { AppContainer } from './styles';
 
 const App = () => {
-  return <AppContainer>Columns will go here</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Generate app scaffold" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Learn TypeScript" />
+      </Column>
+      <Column text="Done">
+        <Card text="Begin to use static typing" />
+      </Column>
+      <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
+    </AppContainer>
+  );
 };
 
 export default App;
